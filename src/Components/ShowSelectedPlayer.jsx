4121de23e16bc2de6f5593c96f2e287840d04e46
neya-mul <neyamulislam946@gmail.com>
2026-03-29 11:28:56 +0600

    select button is now working properly

--- src/Components/ShowSelectedPlayer.jsx
+++ src/Components/ShowSelectedPlayer.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { MdDelete } from 'react-icons/md'
 
-export default function ShowSelectedPlayer() {
+export default function ShowSelectedPlayer({selectedPlayer}) {
   return (
-    <div>
+    <div className='flex justify-between border border-gray-300 items-center p-4 rounded-2xl'> 
+
+        <div className='flex items-center gap-4'>
+            <img src={selectedPlayer.image} alt=""  className='h-20 w-fit rounded-xl'/>
+            <div>
+                <h1 className='text-xl font-bold'>{selectedPlayer.name}</h1>
+                <p>{selectedPlayer.battingStyle}</p>
+            </div>
+        </div>
+
+        <button className='btn text-red-400'> <MdDelete /></button>
 
 
-        
+
     </div>
   )
 }
