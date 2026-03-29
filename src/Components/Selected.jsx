c71c11dd2300760938d5a48eeb17c3078b434460
neya-mul <neyamulislam946@gmail.com>
2026-03-29 19:34:03 +0600

    margin fixed

--- src/Components/Selected.jsx
+++ src/Components/Selected.jsx
@@ -6,7 +6,7 @@ export default function Selected({ selectedPlayer, setSelectedPlayer, coinValue,
     // console.log(selectedPlayer)
     return (
 
-        <div className="space-y-4">
+        <div className="space-y-4 mb-6 md:mb-0">
             {selectedPlayer.length === 0 ? <NoPlayer></NoPlayer> :
                 selectedPlayer.map(player => <ShowSelectedPlayer player={player} key={player.name} selectedPlayer={selectedPlayer} setSelectedPlayer={setSelectedPlayer} coinValue={coinValue} setCoinValue={setCoinValue}></ShowSelectedPlayer>)
 
